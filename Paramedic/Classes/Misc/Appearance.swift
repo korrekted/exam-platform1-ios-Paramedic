763be40def5dc696f782f88176dca403f8ff1259
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-11-08 15:54:56 +0300

    build 1

---
+++ Paramedic/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 250, green: 251, blue: 255)
+    
+    static let mainColor = UIColor(integralRed: 71, green: 79, blue: 234)
+    static let mainColorAlpha = UIColor(integralRed: 71, green: 79, blue: 234, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 229, green: 79, blue: 72)
+    
+    static let progress1Color = UIColor(integralRed: 229, green: 79, blue: 72)
+    static let progress2Color = UIColor(integralRed: 71, green: 79, blue: 234)
+    static let progress3Color = UIColor(integralRed: 11, green: 7, blue: 85)
+}
---
+++ StatsWidgets/Widgets/Widgets/MediumRateWidget.swift
@@ -0,0 +1,52 @@
+//
+//  SmallRateWidget.swift
+//  StatsWidgetsExtension
+//
+//  Created by Andrey Chernyshev on 12.07.2021.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct MediumRateWidget: Widget {
+    let placeholder = StatsContent(date: Date(),
+                                   passRate: 75,
+                                   testTaken: 0,
+                                   testsTakenNum: 10,
+                                   correctAnswers: 0,
+                                   correctAnswersNum: 374,
+                                   questionsTaken: 0,
+                                   answeredQuestions: 400)
+    
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: "MediumRateWidget", provider: Provider(placeholder: placeholder)) { entry in
+            VStack(alignment: .leading) {
+                VStack(alignment: .center) {
+                    PassRateView(title: "Widgets.PassRate".localized,
+                                 progressFontSize: 19.scale,
+                                 progress: entry.passRate / 100,
+                                 titleColor: Color.make(11, green: 7, blue: 85),
+                                 progressColor: Color.make(71, green: 79, blue: 234))
+                    Spacer()
+                    HStack() {
+                        MediumVerticalRateView(count: entry.testsTakenNum,
+                                               title: "Widgets.TestsTaken".localized,
+                                               color: Color.make(11, green: 7, blue: 85))
+                        Spacer()
+                        MediumVerticalRateView(count: entry.correctAnswersNum,
+                                               title: "Widgets.CorrectAnswers".localized,
+                                               color: Color.make(71, green: 79, blue: 234))
+                        Spacer()
+                        MediumVerticalRateView(count: entry.answeredQuestions,
+                                               title: "Widgets.QuestionsTaken".localized,
+                                               color: Color.make(229, green: 79, blue: 72))
+                    }
+                }
+                .padding(16)
+            }
+        }
+        .supportedFamilies([.systemMedium])
+        .configurationDisplayName("Widgets.Medium.Name".localized)
+        .description("Widgets.Medium.Description".localized)
+    }
+}
