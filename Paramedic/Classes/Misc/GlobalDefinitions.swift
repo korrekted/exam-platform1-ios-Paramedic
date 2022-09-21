2ca54722dd73a444d60ade8f04d0a3db4dfa2def
akonst17 <akonst17@gmail.com>
2022-09-21 15:40:41 +0300

    интеграция sdk 2.0; новые урлы; build 8

--- Paramedic/Classes/Misc/GlobalDefinitions.swift
+++ Paramedic/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "wObEoPZZfCYDglHy"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "1wAVPj6joSkrQGb9"
 
     static let applicationTag = "EXAM_PARAMEDIC"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "1wAVPj6joSkrQGb9"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "1wDZ7cREK3jy5gLT"
 
     static let appStoreUrl = "https://apps.apple.com/app/paramedic-exam-prep/id1591859090"
     static let appleAppID = "1591859090"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
